2556e445d14480c32c49add8d97a561c4af71ac9
toroboltan <jeronimodeabreu@live.com>
2020-06-29 21:49:25 -0500

    date_setter_gui_v01-commit-003 charts buttons added to gui

--- date_setter_gui_v01.py
+++ date_setter_gui_v01.py
@@ -19,20 +19,14 @@ import sys
 import webbrowser
 from etfutils import TktScreeenTable
 
+# Environment variables
 script_path = r'C:\EclipseWorkspaces\csse120\kirk_tools'
 script_list = ['date_setter.py']
 tktPath = r'D:\jeronimo\trading\etf'
 sectorsOverWindow = (157,28)
 sectorsPerfWindow = (190,28)
-
 lineStartToChange1 = 'new_date =' 
 lineStartToChange2 = 'old_date_header ='
-
-# Sectors List
-sectorsTktList = ['IWM','XLF','EEM','XLE','XLK',
-                  'XLV','IYT','XLU','XLI','XLY',
-                  'IYR','XLP','XLB','TLT','GLD',
-                  'UUP','RTH','IYZ','SMH','DBC','USO']
 
 # Parameters to be used for Performance Table
 performanceTable = "Performance"
@@ -42,16 +36,46 @@ perf13w = "-perf13w"
 perf26w = "-perf26w"
 perf52w = "-perf52w"
 perfytd = "-perfytd"
+buttonSecList = ['Sectors Daily',
+                 'Sectors 1W',
+                 'Sectors 4W',
+                 'Sectors 13W',
+                 'Sectors 26W',
+                 'Sectors 52W',
+                 'Sectors YTD']
+buttonChtList = ['Sector ETF',
+                 'Simple Breakout Scan STK',
+                 'Simple Breakout Scan ETF',
+                 'New High STK',
+                 'New High ETF',
+                 'FATMAN',
+                 'FAGANMSN',
+                 'Long Breakout Setup ETF',
+                 'Long Breakout Setup STK']
+sectorsTktList = ['IWM','XLF','EEM','XLE','XLK',
+                  'XLV','IYT','XLU','XLI','XLY',
+                  'XLV','IYT','XLU','XLI','XLY',
+                  'IYR','XLP','XLB','TLT','GLD',
+                  'IYR','XLP','XLB','TLT','GLD',
+                  'UUP','RTH','IYZ','SMH','DBC','USO']
 
 # Parameters to be used for Overview Table
 overviewTable = "Overview"
 change = "-change"
 
-# Webpage Links
+# Webpages Links
 etfPerLnk = "https://www.etfscreen.com/performance.php?wl=0&s=Rtn-1d%7Cdesc&t=6&d=i&ftS=yes&ftL=no&vFf=dolVol21&vFl=gt&vFv=500000&udc=default&d=e"
+etfSecCht = "https://www.finviz.com/screener.ashx?v=351&ft=4&t=IWM,XLF,EEM,XLE,XLK,XLV,IYT,XLU,XLI,XLY,IYR,XLP,XLB,TLT,GLD,UUP,RTH,IYZ,SMH,DBC,USO&o=-change"
+stkBrkCht = "https://www.finviz.com/screener.ashx?v=351&f=ind_stocksonly,sh_avgvol_o100,sh_curvol_o500,sh_price_o5,ta_change_u2,ta_changeopen_u3,ta_highlow52w_nh,ta_perf_dup&ft=4&ta=0&o=-change"
+etfBrkCht = "https://www.finviz.com/screener.ashx?v=111&f=ind_exchangetradedfund,sh_avgvol_o100,sh_curvol_o500,sh_price_o5,ta_changeopen_u3,ta_highlow52w_nh,ta_perf_dup&ft=4&ta=0&o=-change"
+etfNewHighCht = "https://www.finviz.com/screener.ashx?v=351&f=ind_exchangetradedfund,sh_avgvol_o100,ta_change_u2,ta_highlow52w_nh&ft=4&o=-change"
+stkNewHighCht ="https://www.finviz.com/screener.ashx?v=351&f=ind_stocksonly,sh_avgvol_o100,ta_change_u2,ta_highlow52w_nh&ft=4&o=-change"
+fatmanCht = "https://www.finviz.com/screener.ashx?v=351&t=AMZN,TSLA,MSFT,GOOGL,NFLX,FB&ta=0&o=perf1w"
+faganmsmCht = "https://www.finviz.com/screener.ashx?v=351&t=FB,AAPL,GOOGL,AMZN,NFLX,MSFT,SBUX,NKE&ta=0"
+etfLongBrkCht = "https://www.finviz.com/screener.ashx?v=351&f=ind_exchangetradedfund,sh_avgvol_o400,sh_price_o5,ta_averagetruerange_o1.5,ta_change_u2,ta_highlow20d_b0to3h,ta_highlow50d_b0to3h,ta_sma20_pa,ta_sma200_pa,ta_sma50_pa&ft=4&ta=0&o=-change"
+stkLongBrkCht = "https://www.finviz.com/screener.ashx?v=351&f=ind_stocksonly,sh_avgvol_o400,sh_price_o5,ta_averagetruerange_o1.5,ta_change_u2,ta_highlow20d_b0to3h,ta_highlow50d_b0to3h,ta_sma20_pa,ta_sma200_pa,ta_sma50_pa&ft=4&ta=0&o=-change"
 
 # Functions
-
 def ScreenerArgumnets(eventPressed): 
     switcher = { 
         'Sectors Daily': [overviewTable, change], 
@@ -68,12 +92,30 @@ def ScreenerArgumnets(eventPressed):
     # be assigned as default value of passed argument 
     return switcher.get(eventPressed, "nothing")
 
-
 def openweb(browser="", sites=[]):
     if browser == "chrome":
         chromedir= 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
     for site in sites:
         webbrowser.get(chromedir).open(site)
+
+
+def chartsArgumnets(eventPressed): 
+    switcher = {
+        'Sector ETF': etfSecCht,
+        'Simple Breakout Scan ETF': etfBrkCht,
+        'Simple Breakout Scan STK': stkBrkCht,
+        'New High ETF': etfNewHighCht,
+        'New High STK': stkNewHighCht,
+        'FATMAN': fatmanCht,
+        'FAGANMSN': faganmsmCht,
+        'Long Breakout Setup ETF': etfLongBrkCht,
+        'Long Breakout Setup STK': stkLongBrkCht, 
+    }
+    # get() method of dictionary data type returns  
+    # value of passed argument if it is present  
+    # in dictionary otherwise second argument will 
+    # be assigned as default value of passed argument 
+    return switcher.get(eventPressed, "nothing")
         
 # Main Program
 
@@ -81,7 +123,7 @@ sg.theme('BluePurple')
 
 layout = [[sg.Text('ETF Performance:'), sg.Text(size=(40,1), key='-OUTPUT-')],
           [sg.Input(key='-IN-')],
-          [sg.Button('Open etfscreen age'), sg.Button('File'), sg.Button('Execute'), sg.Button('Exit')], 
+          [sg.Button('Open etfscreen age'), sg.Button('File'), sg.Button('Execute')], 
           [sg.Text('Sectors Performance')], 
           [sg.Button('Sectors Daily'), 
            sg.Button('Sectors 1W'), 
@@ -89,7 +131,19 @@ layout = [[sg.Text('ETF Performance:'), sg.Text(size=(40,1), key='-OUTPUT-')],
            sg.Button('Sectors 13W'), 
            sg.Button('Sectors 26W'), 
            sg.Button('Sectors 52W'),  
-           sg.Button('Sectors YTD')]]
+           sg.Button('Sectors YTD')],
+          [sg.Text('Charts')],
+          [sg.Button('Sector ETF'),
+           sg.Button('Simple Breakout Scan ETF'),
+           sg.Button('Simple Breakout Scan STK'),
+           sg.Button('New High ETF'),
+           sg.Button('New High STK'),
+           sg.Button('Long Breakout Setup ETF'),
+           sg.Button('Long Breakout Setup STK'),
+           sg.Button('FATMAN'),
+           sg.Button('FAGANMSN')],
+          [sg.Text('Close')],
+          [sg.Button('Exit')]]
 
 window = sg.Window('Pattern 2B', layout)
 
@@ -97,11 +151,14 @@ while True:  # Event Loop
     event, values = window.read()
     print(event, values)
     
-    if event in  (None, 'Exit'):
+    if event in (None, 'Exit'):
         break
     
     if event == 'Open etfscreen age':
         openweb("chrome", [etfPerLnk])
+    
+    if event in buttonChtList:
+        openweb("chrome", [chartsArgumnets(event)])
     
     if event == 'File':
         fname = sys.argv[1] if len(sys.argv) > 1 else sg.popup_get_file('Document to open', initial_folder=tktPath)
@@ -152,7 +209,8 @@ while True:  # Event Loop
             for script_file in script_list:
                 os.system('python ' + script_file)
 
-    if (event != 'File') and (event != 'Execute') and (event != 'Open etfscreen age'):
+    # Performance printing on Windows
+    if (event in buttonSecList):
         paramList = ScreenerArgumnets(event)
         print(paramList)
         if len(paramList) > 1:
