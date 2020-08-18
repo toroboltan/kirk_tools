5648009ada50e508772d9d639709f245de6005a0
toroboltan <jeronimodeabreu@live.com>
2020-08-17 23:40:32 -0500

    tradingRecordTable.py - open table and read it

--- tradingRecordTable.py
+++ tradingRecordTable.py
@@ -7,7 +7,17 @@ Created on Mon Aug 17 23:04:22 2020
 
 import PySimpleGUI as sg
 import os
-import sys
+import pandas as pd
+
+
+## Functions
+
+def openTradingTable(tablePath, tableFile, tableSheet):
+    # Reading the file
+    os.chdir(tablePath)
+    df = pd.read_excel(tableFile, sheet_name=tableSheet)
+    return df
+
 
 ## File information
 tablePath = r'C:\Users\jeron\Google Drive\trading\kirk\2019\active trading'
@@ -17,52 +27,18 @@ tableSheet = "TKT_TT_PY_2019_MST"
 ## PySimpleGUI Parameters
 sectorsOverWindow = (157,28)
 sectorsPerfWindow = (190,28)
+buttonTradingTable = 'Open Trading Table'
 
 
 layout = [[sg.Text('ETF Performance:'), sg.Text(size=(40,1), key='-OUTPUT-')],
-          [sg.Input(key='-IN-')],
-          [sg.Button('Open etfscreen age'), sg.Button('File'), sg.Button('Execute')], 
-          [sg.Text('Sectors Performance')], 
-          [sg.Button('Sectors Daily'), 
-           sg.Button('Sectors 1W'), 
-           sg.Button('Sectors 4W'),  
-           sg.Button('Sectors 13W'), 
-           sg.Button('Sectors 26W'), 
-           sg.Button('Sectors 52W'),  
-           sg.Button('Sectors YTD')],
-          [sg.Text('Charts')],
-          [sg.Button('US Markets'),
-           sg.Button('Sector ETF'),
-           sg.Button('ETF Perf Daily'),
-           sg.Button('ETF Perf Weekly'),         
-           sg.Button('Simple Breakout Scan ETF'),
-           sg.Button('Simple Breakout Scan STK'),
-           sg.Button('New High ETF'),
-           sg.Button('New High STK'),
-           sg.Button('Long Breakout Setup ETF'),
-           sg.Button('Long Breakout Setup STK'),
-           sg.Button('FATGANMSN')],
-          [sg.Text('*** Candidates ***')],
-          [sg.Text('From US Markets :'), sg.InputText(key='etfUsMarkets')],
-          [sg.Text('From Sectors :'), sg.InputText(key='etfSecCht')],
-          [sg.Text('From ETF Performance Daily :'), sg.InputText(key='etfPerfDaily')],
-          [sg.Text('From ETF Performance Weekly :'), sg.InputText(key='etfPerfWeekly')],
-          [sg.Text('From Simple Breakout ETF :'), sg.InputText(key='etfBrkCht')],
-          [sg.Text('From Simple Breakout STK :'), sg.InputText(key='stkBrkCht')],
-          [sg.Text('From New High ETF :'), sg.InputText(key='etfNewHighCht')],
-          [sg.Text('From New High STK :'), sg.InputText(key='stkNewHighCht')],
-          [sg.Text('From Long Breakout Setup ETF :'), sg.InputText(key='etfLongBrkCht')],
-          [sg.Text('From Long Breakout setup STK :'), sg.InputText(key='stkLongBrkCht')],
-          [sg.Text('From FATGANMSN :'), sg.InputText(key='fatganmsn')],
-          [sg.Text('******************')],
-          [sg.Button(buttonProcess)],
+          [sg.Button(buttonTradingTable)], 
           [sg.Button('Exit')]]
 
 
 
 # Main Program
 
-window = sg.Window('TorolGui', layout)
+window = sg.Window('tradingTableReader', layout)
 
 while True:  # Event Loop
     event, values = window.read()
@@ -71,7 +47,7 @@ while True:  # Event Loop
     if event in (None, 'Exit'):
         break
     
-    if event == 'Open etfscreen age':
-        openweb("chrome", [etfPerLnk])
+    if event == buttonTradingTable:
+        dfTrading = openTradingTable(tablePath, tableFile, tableSheet)
             
 window.close()
